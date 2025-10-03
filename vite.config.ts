3e613e6202cefd5ce49b92a7eef1835f5e73792c
Sushmita Aich <aichsushmita08@gmail.com>
2025-10-03 15:42:14 +0530

    Deployment update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/RiverRevive/", // 👈 Required for GitHub Pages
+  base: "/RiverRevive_2/", // 👈 Required for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
