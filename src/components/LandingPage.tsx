fe658d7aecf10ebc00e6b9c877d9c923287e922e
Sushmita Aich <aichsushmita08@gmail.com>
2025-10-12 00:13:07 +0530

    Scrollabe Bg img

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -29,7 +29,7 @@ export default function LandingPage({ onRoleSelect, onSignUp }: LandingPageProps
         className="relative min-h-[60vh] bg-cover bg-center bg-no-repeat"
         style={{
           backgroundImage: `url('./Drone_background_home.jpg')`,
-          backgroundAttachment: 'scroll',
+          backgroundSize: '100% 100%',
         }}
       >
         {/* Overlay */}
